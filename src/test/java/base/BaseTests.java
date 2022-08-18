ded060fff55020eeca6690ac209396a3e52ce382
OliverSpinks <oliver-spinks@hotmail.com>
2022-08-18 11:14:11 +0100

    began to utilise finding web elements features for my base test

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,17 +1,28 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
 
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-        System.out.println(driver.getTitle());
+
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("Shifting Content")).click();
+        driver.findElement(By.linkText(("Example 1: Menu Element"))).click();
+        List<WebElement> menuItems = driver.findElements(By.tagName("li"));
+        System.out.println("Number of menu items: " + menuItems.size());
+
+        driver.quit();
     }
 
     public static void main(String args[]){
